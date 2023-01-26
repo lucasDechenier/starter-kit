22dfee529b14c8b4fbcfc8ae9a604b80be624757
lucasDechenier <lucas.dechenieroliveira@hotmail.com>
2023-01-25 21:05:17 -0300

    feat: :sparkles: Adiciona FontAwesome

--- src/main.js
+++ src/main.js
@@ -2,9 +2,19 @@ import { createApp } from 'vue'
 import '@/style.css'
 import App from '@/App.vue'
 
+const app = createApp(App)
+
 // Vuetify
 import vuetify from '@/plugins/vuetify'
+
+// Plugins
 import router from '@/plugins/router'
 import i18n from '@/plugins/i18n'
 
-createApp(App).use(vuetify).use(i18n).use(router).mount('#app')
+
+
+
+app.use(vuetify)
+.use(i18n)
+.use(router)
+.mount('#app')
--- src/plugins/vuetify/index.js
+++ src/plugins/vuetify/index.js
@@ -1,11 +1,21 @@
+import '@fortawesome/fontawesome-free/css/all.css' 
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
+import { aliases, fa } from 'vuetify/iconsets/fa'
+
 import { pt, en, es } from 'vuetify/locale'
 
 const vuetify = createVuetify({
+  icons: {
+    defaultSet: 'fa',
+    aliases,
+    sets: {
+      fa,
+    }
+  },
   locale: {
     locale: 'pt',
     fallback: 'en',
